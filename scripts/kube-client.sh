7a0357bd6ddb5dd773522cd9c400a91d3032bf5a
dkwasny <dkwasny67@gmail.com>
2019-05-19 00:09:56 -0400

    Small DNS changes

--- scripts/kube-client.sh
+++ scripts/kube-client.sh
@@ -11,7 +11,6 @@ cp /vagrant/secrets/ca.pem /etc/secrets/ca.pem;
 cp /vagrant/secrets/kube-client.pem /etc/secrets/host.pem;
 cp /vagrant/secrets/kube-client.key /etc/secrets/host.key;
 cp /vagrant/secrets/kube-admin.* /etc/secrets/;
-cp /vagrant/secrets/kube-user.* /etc/secrets/;
 cp /vagrant/secrets/kwas-dashboard.* /etc/secrets/;
 chmod 644 /etc/secrets/*;
 
